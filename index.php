0786b8998ef1f1e48edaa2103536ee1092cb4254
sharathkv <sharathkv@dckap>
2022-04-27 19:46:07 +0530

    create deactivated subcat manufacturers littelfuse/protection-relay-control/pump-controls/PC-XXX-LLC-CZ-PC-XXX-LLC-GM

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ curl_setopt_array($curl, [
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => "{ \n  \"parent_id\": 704,\n  \"name\": \"201-100-SLD\",\n  \"description\": \"\",\n  \"views\": 1050,\n  \"sort_order\": 0,\n  \"page_title\": \"201-100-SLD\",\n  \"search_keywords\": \"\",\n  \"meta_keywords\": [\n    \"\"\n  ],\n  \"meta_description\": \"\",\n  \"layout_file\": \"default.html\",\n  \"is_visible\": true,\n  \"default_product_sort\": \"use_store_settings\",\n  \"image_url\": \"https://cdn11.bigcommerce.com/s-ca10qrhzok/product_images/uploaded_images/productdefault.gif\",\n  \"custom_url\": {\n    \"url\": \"/products/deactivated-categories/manufacturers/littelfuse/protection-relay-control/protection-relay-control-accessories/201-100-SLD\",\n \"is_customized\": true\n  }\n}",
+  CURLOPT_POSTFIELDS => "{ \n  \"parent_id\": 704,\n  \"name\": \"PC-XXX-LLC-CZ/PC-XXX-LLC-GM\",\n  \"description\": \"\",\n  \"views\": 1050,\n  \"sort_order\": 0,\n  \"page_title\": \"PC-XXX-LLC-CZ/PC-XXX-LLC-GM\",\n  \"search_keywords\": \"\",\n  \"meta_keywords\": [\n    \"\"\n  ],\n  \"meta_description\": \"\",\n  \"layout_file\": \"default.html\",\n  \"is_visible\": true,\n  \"default_product_sort\": \"use_store_settings\",\n  \"image_url\": \"https://cdn11.bigcommerce.com/s-ca10qrhzok/product_images/uploaded_images/productdefault.gif\",\n  \"custom_url\": {\n    \"url\": \"/products/deactivated-categories/manufacturers/littelfuse/protection-relay-control/pump-controls/PC-XXX-LLC-CZ-PC-XXX-LLC-GM\",\n \"is_customized\": true\n  }\n}",
   CURLOPT_HTTPHEADER => [
     "Accept: application/json",
     "Content-Type: application/json",
